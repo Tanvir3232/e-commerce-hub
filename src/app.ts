3e458887a750e83e965e1d466bc693459e2c0e10
Tanvir3232 <tanvirahmadstudent@gmail.com>
2024-05-24 15:49:21 +0600

    deloy on vecel and implement not found route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 import { OrderRoutes } from './app/modules/order/order.route';
 import { ProductRoutes } from './app/modules/product/product.route';
 const app: Application = express();
@@ -13,4 +13,13 @@ app.use('/api/orders', OrderRoutes)
 app.get("/", (req: Request, res: Response) => {
     res.send("Welcome to ecommerce products hub");
 })
+
+// Handle invalid paths
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.status(404).json({
+        success: false,
+        message: "Route not found. Please check the URL and try again."
+    });
+});
+
 export default app;
